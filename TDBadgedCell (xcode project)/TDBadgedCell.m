f1cce183fe57b5a6ae587c5381b1f7b68ee376e7
Dallas Brown <dallas@tesseract.local>
2010-01-16 10:28:45 -0700

    Update to make cells with long text not collide with badges

--- TDBadgedCell (xcode project)/TDBadgedCell.m
+++ TDBadgedCell (xcode project)/TDBadgedCell.m
@@ -151,7 +151,14 @@
 		[self.badge setFrame:badgeframe];
 		[badge setBadgeNumber:self.badgeNumber];
 		[badge setParent:self];
-		
+
+		if ((self.textLabel.frame.origin.x + self.textLabel.frame.size.width) >= badgeframe.origin.x)
+		{
+			CGFloat badgeWidth = self.textLabel.frame.size.width - badgeframe.size.width - 10.0;
+
+			self.textLabel.frame = CGRectMake(self.textLabel.frame.origin.x, self.textLabel.frame.origin.y, badgeWidth, self.textLabel.frame.size.height);
+		}
+
 		//set badge highlighted colours or use defaults
 		if(self.badgeColorHighlighted)
 			badge.badgeColorHighlighted = self.badgeColorHighlighted;
